3629995b3c5d4803a0736f62ea2714c6e6b91378
htluu <htluu@ucsd.edu>
2026-02-12 17:30:50 -0800

    hello world ex

--- ros2-depthai-camera-node/setup.py
+++ ros2-depthai-camera-node/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 package_name = 'ros2-depthai-camera-node'
+python_module_name = 'ros2_depthai_camera_node'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[python_module_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -20,6 +21,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ros2-depthai-camera-node = ros2_depthai_camera_node.node:main',
         ],
     },
 )
