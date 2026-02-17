65448824748e2e0b811182e8cf76108f72814e94
htluu <htluu@ucsd.edu>
2026-02-17 12:50:28 -0800

    fix(step3): install config assets and runtime deps

--- ros2_depthai_package/setup.py
+++ ros2_depthai_package/setup.py
@@ -1,16 +1,34 @@
+from glob import glob
+
 from setuptools import setup
 
 package_name = 'ros2_depthai_package'
+
+data_files = [
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+]
+
+config_files = sorted(glob('config/*.yaml'))
+if config_files:
+    data_files.append(('share/' + package_name + '/config', config_files))
+
+profile_files = sorted(glob('config/profiles/*.yaml'))
+if profile_files:
+    data_files.append(
+        ('share/' + package_name + '/config/profiles', profile_files)
+    )
+
+launch_files = sorted(glob('launch/*.launch.py'))
+if launch_files:
+    data_files.append(('share/' + package_name + '/launch', launch_files))
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jetson',
